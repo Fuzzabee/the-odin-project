f7b5f0de7efc9f8f6638c13ce5323312c8c87ec3
James Baker <jamesjbaker@tutanota.com>
2024-05-19 03:49:58 -0400

    Complete Variables and Operators

---
+++ lessons/foundations/js-basics/variables-and-operators/assignment.js
@@ -0,0 +1,21 @@
+// 1
+console.log("1. " + ( 23 + 97 ) );
+
+// 2
+console.log("2. " + ( 1 + 2 + 3 + 4 + 5 + 6 ) );
+
+// 3
+console.log("3. " + ( (4 + 6 + 9) / 77 ) );
+
+// 4
+let a = 10;
+console.log("4. " + a);
+console.log("4. " + ( 9 * a ));
+let b = 7 * a;
+console.log("4. " + b);
+
+// 5
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+console.log("5. " + percentage);
